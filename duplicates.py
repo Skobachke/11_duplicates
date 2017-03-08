a738d371e5c8fd3f7a5480fa5b14b68ac231c5ce
Skobachke <skobachke@gmail.com>
2017-03-08 22:46:11 +0300

    Добавлен Readme, изменена структура кода

--- duplicates.py
+++ duplicates.py
@@ -1,21 +1,26 @@
 import os
 
+def file_list(directory):
+    global equal
+    pathlist = []
+    for i in os.walk(directory):
+        for j in i[2]:
+            pathlist.append(os.path.join(str(i[0]), str(j)))
+
+    for i in range(len(pathlist)-1):
+        for j in range(i+1,len(pathlist)):
+            if pathlist[i] != pathlist[j]:
+                are_files_duplicates(pathlist[i], pathlist[j])
+    if equal == 0:
+        print (u'Совпадений не найдено!')
+
 def are_files_duplicates(file_path1, file_path_2):
     if (os.path.split(file_path1)[1] == os.path.split(file_path_2)[1]) and os.path.getsize(file_path1) == os.path.getsize(file_path_2):
-        print (u'Совпадение файлов {} и {}!'.format(file_path1, file_path_2))
+        global equal
+        equal += 1
+        print (u'{}. Совпадение файлов {} и {}!'.format(equal, file_path1, file_path_2))
 
 if __name__ == '__main__':
-    directory = '.'
-    contdir, listdir = [], []
-
-    for i in os.walk(directory):
-        contdir.append(i)
-
-    for i in contdir:
-        for j in i[2]:
-            listdir.append(os.path.join(str(i[0]), str(j)))
-
-    for i in range(len(listdir)-1):
-        for j in range(i+1,len(listdir)):
-            if listdir[i] != listdir[j]:
-                are_files_duplicates(listdir[i], listdir[j])
+    directory = input(u'Введите путь к папке:')
+    equal = 0
+    file_list(directory)
